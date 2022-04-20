d91c7def0138cda5241bba02510b557db6a2cb0e
Mac <hung.mac@dbschenker.com>
2022-04-20 09:12:36 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ShortCodeListComponent} from './short-code-list/short-code-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'list',
+    component: ShortCodeListComponent,
+  },
+  {
+    path: 'add',
+    loadChildren: () => import('./short-url/short-url.module').then(m => m.ShortUrlModule),
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
